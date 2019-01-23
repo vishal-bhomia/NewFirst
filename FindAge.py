92cccd1ab0b0d0feb75df4d14d9ff200d3f9938a
VBhomia <vbhomia@evolenthealth.com>
2019-01-23 18:56:48 +0530

    Comments added wherever required
    Signed-off-by: VBhomia <vbhomia@evolenthealth.com>

--- FindAge.py
+++ FindAge.py
@@ -2,22 +2,21 @@ import json
 from datetime import date
 from datetime import datetime
 
-
+i=0
+#function to calculate age when DOB is provided
 def calculate_age(DOB):
     today = date.today()
     return today.year - DOB.year - ((today.month, today.day) < (DOB.month, DOB.day))
 
-
-
+#loading existiong JSON into an object jsonObj
 copy = open("Members.json","r")
 memData = copy.read()
 jsonObj = json.loads(memData)
 
-i=0
+#creating a JSON file and populating it with existing JSON appended with AGE
 with open('Members_Copy.json', 'w') as newFile:
     while(i < len(jsonObj)):
         jsonObj[i]["Age"] = calculate_age(datetime.strptime(jsonObj[i]["DOB"], '%Y-%m-%d'))
-        #print(jsonObj[i])
         i += 1
     json.dump(jsonObj, newFile, indent=4)
 print("Completed")
